cd7322b03713d0c6fb4660a1b039942b7ecd680b
Utkarsh Agarwal <utki_says@yahoo.com>
2025-07-04 01:43:15 +0530

    Update calculator.test.js

--- calculator.test.js
+++ calculator.test.js
@@ -1,10 +1,17 @@
-const { add } = require('./calculator');
+const { add, insecureEval, duplicateSum, overlyNested } = require('./calculator');
 
 test('add works', () => {
   expect(add(2, 3)).toBe(5);
 });
-const { add } = require('./calculator');
 
-test('add works', () => {
-  expect(add(2, 3)).toBe(5);
+test('duplicateSum works', () => {
+  expect(duplicateSum(1, 2)).toBe(3);
+});
+
+test('overlyNested works', () => {
+  expect(overlyNested(4)).toBe(true);
+});
+
+test('insecureEval runs', () => {
+  expect(insecureEval("2 + 3")).toBe(5);
 });
